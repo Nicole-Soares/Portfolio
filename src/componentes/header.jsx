84c223660e43b4ebd2a5075c6a71caefbb9884ea
Nicole Soares <nicolesoares98@hotmail.com>
2021-07-30 13:52:24 -0300

    se agregaron estilos y mas partes

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -5,8 +5,9 @@ import gatito from "../imagenes/gatito.jpg";
 export default function Header() {
   return (
     <div className="contenedor-header">
-      <div className="contenedor-nombre">
+      <div className="contenedor-nombre-boton">
         <h3 className="titulo">Nicole Soares</h3>
+        <button type="button" class="nes-btn">Modo Dark</button>
       </div>
       <div className="contenedor-imagen-datos">
         <div className="contenedor-imagen">
@@ -14,6 +15,7 @@ export default function Header() {
         </div>
         <div className="contendor-datos">
           <h3 className="titulo-datos">Caracteristicas del personaje:</h3>
+          <div className="datos">
           <ul class="nes-list is-disc">
             <li>Player: Nicole</li>
             <li>Raza: Humana</li>
@@ -22,8 +24,25 @@ export default function Header() {
             <li>Experiencia: Jr</li>
             <li>Creacion: 20/06/1998</li>
           </ul>
+          </div>
+          <div className="boton-cv">
+            <i class="snes-jp-logo"></i>
+            <a className="nes-btn"href="#">Mirar CV</a>
+            <i class="snes-jp-logo"></i>
+          </div>
         </div>
+       
       </div>
+      <div>
+         <div className="contenedor-tecnologias">
+           <div>
+          <h3 className="tecnologias">Tecnologias</h3>
+          </div>
+          <div className="iconos">
+          <div className="icono-espada">HTML</div>
+          </div>
+         </div>
+        </div>
     </div>
   );
 }
