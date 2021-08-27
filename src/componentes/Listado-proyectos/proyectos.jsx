6cbf0fc549a3a0b1777a0be0dc908bd45e958e11
Nicole Soares <nicolesoares98@hotmail.com>
2021-08-27 13:27:11 -0300

    agregar proyecto

--- src/componentes/Listado-proyectos/proyectos.jsx
+++ src/componentes/Listado-proyectos/proyectos.jsx
@@ -3,6 +3,7 @@ import "./proyectos.css";
 import proyecto from "../../imagenes-proyectos/proyecto-1.png";
 import proyectoDos from "../../imagenes-proyectos/proyecto-2.png";
 import proyectoTres from "../../imagenes-proyectos/proyecto-3.png";
+import proyectoCuatro from "../../imagenes-proyectos/proyecto-4.png"
 import { AppContext } from "../../AppContext/AppContext";
 
 export default function Proyectos() {
@@ -14,76 +15,121 @@ export default function Proyectos() {
         <h3 className={`titulo ${dark ? "dark" : ""}`}>Proyectos</h3>
       </div>
       <div className="contenedor-proyectos">
-        <div className="proyecto-uno">
+        <div className="proyecto">
           <img
             src={proyecto}
-            width="50%"
+            width="100%"
             height="250px"
             alt="Modelo de pagina"
           />
           <div class="transparente">
-            <h3 class="nombre-del-proyecto-uno">Modelo de pagina web</h3>
-            <p class="descripcion-proyecto-uno">
+            <h3 class="nombre-del-proyecto">Modelo de pagina web</h3>
+            <p class="descripcion-proyecto">
               Clon de una pagina web estatica
             </p>
             <a
               href="https://github.com/Nicole-Soares/PrimerProyecto"
+              target="_blank"
+              rel="noopener noreferrer"
               className="proyecto-link"
             >
               Ver Repositorio
             </a>
-            <a href="https://nicole-soares.github.io/pagina-estatica/" className="proyecto-link">
+            <a
+              href="https://nicole-soares.github.io/pagina-estatica/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
+            >
               Ver Demo
             </a>
           </div>
         </div>
-        <div className="proyecto-dos">
+        <div className="proyecto">
           <img
             src={proyectoDos}
-            width="50%"
+            width="100%"
             height="250px"
             alt="Modelo de pagina"
           />
-          <div class="transparente-dos">
-            <h3 class="nombre-del-proyecto-dos">Modelo de pagina web</h3>
-            <p class="descripcion-proyecto-dos">
+          <div class="transparente">
+            <h3 class="nombre-del-proyecto">Modelo de pagina web</h3>
+            <p class="descripcion-proyecto">
               Clon de una pagina web de reserva de hoteles
             </p>
             <a
               href="https://github.com/Nicole-Soares/pagina-de-hoteles"
-              className="proyecto-link-dos"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
             >
               Ver Repositorio
             </a>
             <a
               href="https://nicole-soares.github.io/pagina-de-hoteles/"
-              className="proyecto-link-dos"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
             >
               Ver Demo
             </a>
           </div>
         </div>
-        <div className="proyecto-tres">
+        <div className="proyecto">
           <img
             src={proyectoTres}
-            width="50%"
+            width="100%"
             height="250px"
             alt="Modelo de pagina"
           />
-          <div className="transparente-tres">
-            <h3 class="nombre-del-proyecto-tres">Modelo de pagina web</h3>
-            <p class="descripcion-proyecto-tres">
+          <div className="transparente">
+            <h3 class="nombre-del-proyecto">Modelo de pagina web</h3>
+            <p class="descripcion-proyecto">
               Clon de una pagina web de gifs
             </p>
             <a
               href="https://github.com/Nicole-Soares/pagina-de-gifs"
-              className="proyecto-link-tres"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
             >
               Ver Repositorio
             </a>
             <a
               href="https://nicole-soares.github.io/pagina-de-gifs/"
-              className="proyecto-link-tres"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
+            >
+              Ver Demo
+            </a>
+          </div>
+        </div>
+        <div className="proyecto">
+          <img
+            src={proyectoCuatro}
+            width="100%"
+            height="250px"
+            alt="Modelo de pagina"
+          />
+          <div class="transparente">
+            <h3 class="nombre-del-proyecto">Modelo de pagina web</h3>
+            <p class="descripcion-proyecto">
+              Clon de una pagina web sobre canjes de puntos
+            </p>
+            <a
+              href="https://github.com/Nicole-Soares/tienda-electronica"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
+            >
+              Ver Repositorio
+            </a>
+            <a
+              href="https://nicole-soares.github.io/tienda-electronica/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="proyecto-link"
             >
               Ver Demo
             </a>
